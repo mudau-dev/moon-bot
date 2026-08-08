4b0ce20c6ba8f655937ec450b66b19471f664ff2
mudau-dev <vhadau.moon@gmail.com>
2026-08-08 10:57:41 +0200

    Update and rename config.js to config.example.js

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = Object.freeze({
-  MENU_IMAGE: process.env.MENU_IMAGE || '',
-  PREFIX: process.env.PREFIX ?? '.',
-  BOT_NAME: process.env.BOT_NAME || 'Moonlight Haven',
-  BOT_JID: process.env.BOT_JID || '',
-});
---
+++ confige.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  BOT_NAME: "",
+  BOT_JID: "@lid", 
+  MENU_IMAGE: "",
+  SESSION_FOLDER: "./sessions",
+  
+PREFIX: process.env.PREFIX,
+  MOONLIGHT_IMAGE: process.env.MOONLIGHT_IMAGE,
+              WEB: process.env.WEB,
+       OWNER_NAME: process.env.OWNER_NAME,
+   FASTIVAL_GROUP: process.env.FASTIVAL_GROUP,
+   COMMUNITY_LINK: process.env.COMMUNITY_LINK,
+         RPG_LINK: process.env.RPG_LINK,
+     CASINO_LINK1: process.env.CASINO_LINK1,
+     CASINO_LINK2: process.env.CASINO_LINK2,
+     DISCORD_LINK: process.env.DISCORD_LINK,
+  };
